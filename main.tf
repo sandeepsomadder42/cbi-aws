f850094bc7a7e638301a57e63796a35419df5950
sandeepsomadder42 <sandeepsomadder42@gmail.com>
2024-08-18 21:21:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "dev_ops_windows" {
   ami           = "ami-07d9456e59793a7d5"  # Replace with a valid Windows AMI ID for your region
   instance_type = "t2.micro"               # Choose the instance type based on your needs
 
-  subnet_id              = aws_subnet.private_subnet_1b.id
+  subnet_id              = aws_subnet.public_subnet_1a.id
   vpc_security_group_ids = [aws_security_group.windows_rdp.id]
 
   key_name                   = "Windowskey.pem"         # Replace with your actual PEM key name
