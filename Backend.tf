e8361337a3666142f221919377bd89144d966d22
sandeepsomadder42 <sandeepsomadder42@gmail.com>
2024-08-18 17:34:18 +0530

    Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-state-sandy-bucket"
-    key = "main"
+    key = "lockID"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table-tf"
   }
